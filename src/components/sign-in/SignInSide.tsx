98cffa536a4c54f09031b3abab830daf68ff1acc
Sebastian Wieland <wielandseb@gmail.com>
2024-10-21 12:59:23 +0200

    Simplify SignInSide component by removing unused imports 💡

--- src/components/sign-in/SignInSide.tsx
+++ src/components/sign-in/SignInSide.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
-import {ThemeProvider} from '@mui/material/styles';
 import SignInCard from './SignInCard';
 import Content from './Content';
-import ToggleColorMode from "../utilities/ToggleColorMode";
 import useTheme from "../theme/useTheme";
 import DefaultLayout from "../theme/DefaultLayout";
 
