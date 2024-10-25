93b6d786abac14fcc979dab0678da08f5a2b75ff
Sebastian Wieland <wielandseb@gmail.com>
2024-10-25 12:29:57 +0200

    Simplify LandingPage component by removing unused imports and code 💻

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,24 +1,19 @@
 import React, {useContext, useState} from 'react';
-import DataTable from "./journal/DataTable";
-import DataCollector, {DataCollectorState} from "./journal/DataCollector";
-import Button from '@mui/material/Button';
-import SignInSide from "./sign-in/SignInSide";
 import DefaultLayout from '../theme/DefaultLayout';
 import {AuthContext} from "../contexts/AuthContext";
+import SignInSide from "./sign-in/SignInSide";
 
 const LandingPage = () => {
-    const [showTable, setShowTable] = useState(false);
     const isLoggedIn = useContext(AuthContext)?.isLoggedIn;
 
-    // if (!isLoggedIn) {
-    //     return <SignInSide/>;
-    // }
+    if (!isLoggedIn) {
+        return <SignInSide/>;
+    }
 
     return (
         <DefaultLayout>
             <div>
                 <h1>Welcome to your Band Journal!</h1>
-                <p>Choose an option:</p>
             </div>
         </DefaultLayout>
 
