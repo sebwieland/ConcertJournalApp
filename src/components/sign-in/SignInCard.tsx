e8dc26d7ca5a568ef0fdb785a9b7d1a3d17e658a
Sebastian Wieland <wielandseb@gmail.com>
2024-12-11 12:52:37 +0100

    Remove forgot password feature and minor formatting changes 💻

--- src/components/sign-in/SignInCard.tsx
+++ src/components/sign-in/SignInCard.tsx
@@ -2,17 +2,14 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import MuiCard from '@mui/material/Card';
-import Checkbox from '@mui/material/Checkbox';
 import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import {styled} from '@mui/material/styles';
 
-import ForgotPassword from './ForgotPassword';
 import {useNavigate} from "react-router-dom";
 import {useLayoutEffect, useState} from "react";
 import useAuth from "../../hooks/useAuth";
@@ -37,12 +34,11 @@ const Card = styled(MuiCard)(({theme}) => ({
 
 export default function SignInCard() {
     const navigate = useNavigate();
-    const { login } = useAuth();
+    const {login} = useAuth();
     const [emailError, setEmailError] = useState(false);
     const [emailErrorMessage, setEmailErrorMessage] = useState('');
     const [passwordError, setPasswordError] = useState(false);
     const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-    const [open, setOpen] = React.useState(false);
     const [signInError, setSignInError] = useState('');
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -54,8 +50,8 @@ export default function SignInCard() {
 
         if (validateInputs()) {
             try {
-                await login({ email, password });
-                navigate('/', { replace: true });
+                await login({email, password});
+                navigate('/', {replace: true});
             } catch (error) {
                 console.error('Error logging in:', error);
                 setSignInError('Invalid email or password. Please try again')
@@ -63,14 +59,6 @@ export default function SignInCard() {
         } else {
             console.error('Invalid inputs');
         }
-    };
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
     };
 
     useLayoutEffect(() => {
@@ -123,7 +111,7 @@ export default function SignInCard() {
                 variant="h4"
                 sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
             >
-            Sign in
+                Sign in
             </Typography>
             <Box
                 component="form"
@@ -173,25 +161,13 @@ export default function SignInCard() {
                         variant="outlined"
                         color={passwordError ? 'error' : 'primary'}
                     />
-                    <Link
-                        component="button"
-                        onClick={handleClickOpen}
-                        variant="body2"
-                        sx={{alignSelf: 'baseline'}}
-                    >
-                        Forgot your password?
-                    </Link>
                 </FormControl>
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary"/>}
-                    label="Remember me"
-                />
-                <ForgotPassword open={open} handleClose={handleClose}/>
+
                 <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>
                     Sign in
                 </Button>
                 {signInError && (
-                    <Typography color="error" sx={{ textAlign: 'center' }}>
+                    <Typography color="error" sx={{textAlign: 'center'}}>
                         {signInError}
                     </Typography>
                 )}
