1cfb0183baa42a48e6cc3798170fe896a65b8360
Sebastian Wieland <wielandseb@gmail.com>
2025-01-24 20:27:51 +0100

    Add debug logs and cleanup useAuthContext useEffect dependencies.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -61,25 +61,43 @@ const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     }, [csrfToken, apiClient, setLoggedOut, setAccessToken, setIsLoggedIn]);
 
     useEffect(() => {
+        console.log('useEffect is running'); // Debug log for useEffect execution
+
         const setupAuth = async () => {
+            console.log('setupAuth is starting'); // Debug log for setupAuth start
+
             try {
                 await fetchCsrfToken();
+                console.log('CSRF token fetched successfully'); // Debug log for CSRF token fetch
+
                 const storedRefreshToken = document.cookie.match(/refreshToken=([^;]*)/)?.[1] || '';
+                console.log('Stored refreshToken:', storedRefreshToken); // Debug log for stored refresh token
+
                 if (storedRefreshToken) {
                     setRefreshToken(storedRefreshToken);
                     await refreshTokenApiCall();
+                    console.log('Refresh token API call completed'); // Debug log for API call completion
                 } else {
                     setLoggedOut();
+                    console.log('No refresh token found, setting logged out'); // Debug log for no refresh token
                 }
             } catch (error) {
                 console.error("Error during setupAuth:", error);
                 setLoggedOut();
+            } finally {
+                setIsLoading(false); // Ensure isLoading is set to false after attempt
             }
         };
         setupAuth();
-        const intervalId = setInterval(refreshTokenApiCall, 2*60*1000); // 2 minutes
-        return () => clearInterval(intervalId);
-    }, [fetchCsrfToken, refreshTokenApiCall, setLoggedOut, setIsLoading, setAccessToken, setIsLoggedIn]);
+
+        const intervalId = setInterval(refreshTokenApiCall, 2 * 60 * 1000); // 2 minutes
+        console.log('Set up interval for token refresh'); // Debug log for setting up interval
+
+        return () => {
+            clearInterval(intervalId);
+            console.log('Interval cleared on component unmount'); // Debug log for clearing interval
+        };
+    }, [fetchCsrfToken, refreshTokenApiCall, setLoggedOut]);
 
     return (
         <AuthContext.Provider value={{
