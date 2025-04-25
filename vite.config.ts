47cd2b0b3ce64f53c6b1df610ee3b2187b40cc35
Sebastian Wieland <wielandseb@gmail.com>
2025-04-25 09:34:52 +0200

    Add sitemap plugin to improve SEO with dynamic routes configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import Sitemap from 'vite-plugin-sitemap'
+
+const dynamicRoutes = [
+    '/new-entry',
+    '/your-journal',
+    '/sign-up',
+    '/sign-in',
+    // Add other paths here
+]
 
 export default defineConfig(async () => ({
     base: '/',
@@ -9,7 +18,12 @@ export default defineConfig(async () => ({
     },
     plugins: [
         react(),
-        tsconfigPaths()
+        tsconfigPaths(),
+        Sitemap({
+            hostname: 'https://concertjournal.com',
+            dynamicRoutes, // pass the function here
+            exclude: ['/secret-page'] // optional exclude
+        })
     ],
     server: {
         port: 3010,
